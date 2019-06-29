ffd5f8b3a8a07fc4e74f1df4a1707227603e36dc
Honglai <hlzhan@thoughtworks.com>
2019-06-29 09:51:03 +0800

    [yangqian] Code clean

--- src/main/java/com/thoughtworks/homework/service/UserService.java
+++ src/main/java/com/thoughtworks/homework/service/UserService.java
@@ -6,7 +6,6 @@ import com.thoughtworks.homework.exception.BaseUserException;
 import com.thoughtworks.homework.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,7 +17,7 @@ public class UserService {
     private UserRepository userRepository;
 
     public UserDTO<Iterable<User>> getAllUsers(){
-        UserDTO<Iterable<User>> u= new UserDTO<Iterable<User>>();
+        UserDTO<Iterable<User>> u= new UserDTO<>();
         u.setCode(200);
         u.setData(userRepository.findAll());
         u.setMessage("数据获取成功！");
@@ -44,11 +43,10 @@ public class UserService {
 
     public UserDTO<User> findUserById (int id) throws BaseUserException {
         Optional<User> user = userRepository.findById(id);
-        //return user.orElseThrow(() -> new EntityNotFoundException("id 为 " + id + " 的用户不存在"));
         if(!user.isPresent()){
             throw new BaseUserException("用户不存在");
         }
-        UserDTO<User> u =new UserDTO<User>();
+        UserDTO<User> u = new UserDTO<>();
         u.setCode(200);
         u.setData(user.get());
         u.setMessage("查找成功");
@@ -65,7 +63,7 @@ public class UserService {
                 if(list.isEmpty()) {
                     u.get().setUsername(user.getUsername());
                     userRepository.save(u.get());
-                    UserDTO<User> u1 = new UserDTO<User>();
+                    UserDTO<User> u1 = new UserDTO<>();
                     u1.setCode(200);
                     u1.setData(u.get());
                     u1.setMessage("用户名修改成功！");
